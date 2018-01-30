f86657b049e0a9b37103de4b6114b5cbbc658885
DenR2D2 <Den_123_456@mail.ru>
2018-01-30 13:27:50 +0300

    0.001
    Camera control
    bad jump

---
+++ platformer/Assets/Scripts/CameraController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject player;
+    public float maxDifference;
+    public float speed = 10f;
+
+    Rigidbody rb;
+    float difference;
+    Vector3 movement;
+    float playerPosition;
+
+
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();         
+        
+    }
+
+
+    void Update()
+    {
+        difference = transform.position.x - player.transform.position.x;
+        if (difference > maxDifference)
+        {
+            playerPosition = player.transform.position.x;
+            Moving(playerPosition);
+        }
+        if (difference < -maxDifference)
+        {
+            playerPosition = player.transform.position.x;
+            Moving(playerPosition);
+        }
+
+    }
+
+    void Moving(float h)
+    {
+        movement.Set(h, 0f, 0f);
+        movement = movement.normalized * speed * Time.deltaTime;
+        rb.MovePosition(transform.position + movement);
+    }
+}
--- platformer/Assets/Scripts/Move.cs
+++ platformer/Assets/Scripts/Move.cs
@@ -6,22 +6,44 @@ public class Move : MonoBehaviour
 {
 
 	public float speed = 10f;
+	//public float jumpRate = 2f;
+    public float jump = 250f;
+    public Transform groundCheck;
+    public LayerMask whatIsGround;
+
 
 	Vector3 movement;
 	Rigidbody rb;
+	float timer;
+    bool isGrounded = false;
+    float groundRadius = 0.5f;
+    
+
 
 	void Awake ()
 	{
 		rb = GetComponent<Rigidbody>();
+        
 	}
+
+	void Update()
+	{
+        if (isGrounded && Input.GetButton("Jump"))
+        {
+            rb.AddForce(new Vector3(0f,jump,0f));
+        }
+        
+    }
 
 	void FixedUpdate ()
 	{
-		float h = Input.GetAxis ("Horizontal");
-		//float v = Input.GetAxis ("Vertical");
-
+        // isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+        isGrounded = Physics.Raycast(groundCheck.position, Vector3.down, groundRadius, whatIsGround);
+        
+		float h = Input.GetAxis ("Horizontal"); 
 		Moving (h);
 	}
+
 
 	void Moving (float h)
 	{
